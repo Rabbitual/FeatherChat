79c59a948dc0dd5e8e9ac74e2d784b54f49d1728
Escad <aeriscapella@outlook.com>
2022-09-30 12:00:18 -0700

    Optimize Gradle setup, use Bukkit:shadowJar to build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,25 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     java
-    apply{
-        id("xyz.mauwh.java-conventions")
-        id("com.github.johnrengelman.shadow") version("7.1.2")
+    id("com.github.johnrengelman.shadow") version("7.1.2")
+}
+
+subprojects {
+    apply(plugin="java")
+    apply(plugin="maven-publish")
+
+    group = "xyz.mauwh.featherchat"
+    version = "1.0-SNAPSHOT"
+
+    java.targetCompatibility = JavaVersion.VERSION_17
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
     }
-}
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation(project(":Api"))
-    implementation(project(":Bukkit"))
-    implementation(project(":FeatherChat"))
-}
-
-tasks.withType<ShadowJar> {
-    relocate("co.aikar", "xyz.mauwh.featherchat.libs.acf")
-    relocate("net.kyori", "xyz.mauwh.featherchat.libs.kyori")
-    relocate("org.yaml.snakeyaml", "xyz.mauwh.featherchat.libs.snakeyaml")
-    archiveBaseName.set("FeatherChat")
-    archiveClassifier.set("")
-    archiveVersion.set("")
+    repositories {
+        mavenCentral()
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        maven("https://repo.aikar.co/content/groups/aikar/")
+    }
 }
